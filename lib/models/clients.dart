d5e001b00a573bdb96cc5cbc54c57e05d85fcf7f
Vilar95 <e.vilar95@gmail.com>
2024-06-06 17:22:39 -0300

    Provider

---
+++ lib/models/clients.dart
@@ -0,0 +1,13 @@
+import 'package:client_control/models/client.dart';
+import 'package:flutter/material.dart';
+
+class Clients extends ChangeNotifier {
+  List<Client> clients;
+
+  Clients({required this.clients});
+
+  void add(Client client) {
+    clients.add(client);
+    notifyListeners();
+  }
+}
